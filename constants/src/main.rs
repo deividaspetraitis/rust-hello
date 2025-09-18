3bf31c2a82454033f20f0d254ecf7b1eeb500f5a
Deividas Petraitis <hi@deividaspetraitis.lt>
2025-09-18 21:13:00 +0300

    Add constants

---
+++ constants/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // It's more readable and easier to write it as following expression
+    // than just a number 10800.
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Three hours in seconds is: {THREE_HOURS_IN_SECONDS}");
+}
